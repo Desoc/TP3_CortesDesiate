90eee42b7efa953815ec072a4753b47a2eb96590
ulicortes <ulisestcortes@gmail.com>
2021-02-20 18:28:41 -0300

    Termine los botones de agregar y editar, tengo que arreglar la funcionalidad del modal de editar y ya esta terminado

--- tp3/src/store/index.js
+++ tp3/src/store/index.js
@@ -10,16 +10,28 @@ export default new Vuex.Store({
     dessertsQ: [],
     dessertsT: [],
     header: ['Postre', 'Tiempo de preparado', 'Cantidad de ingredientes'],
-    show: false
+    show: false,
+    inpuPostre: '',
+    inpuTdp: '',
+    inpuCdi: '',
+    newDessert: [],
+    ids: []
   },
   mutations: {
     traerPostres (state, elementos) {
       setTimeout(function () {
+        for (this.l in elementos) {
+          state.ids.push(elementos[this.l].id)
+        }
         state.desserts = elementos
         state.dessertsSf = elementos
         state.dessertsQ = state.desserts.filter(el => el.cantidadDeIngredientes <= 5)
         state.dessertsT = state.desserts.filter(el => parseInt(el.tiempoDePreparado) >= parseInt('70 minutos'))
         state.show = true
+        for (this.i in elementos) {
+          delete elementos[this.i].id
+        }
+        console.log(state.ids)
       }, 2000)
     }
   },
@@ -29,11 +41,20 @@ export default new Vuex.Store({
         .then((res) => res.json())
         .then((elementos) => {
           commit('traerPostres', elementos)
-          for (this.i in elementos) {
-            delete elementos[this.i].id
-          }
         })
         .catch((error) => console.log(error))
+    },
+    addDessert: function (context, payload) {
+      fetch('https://5fcba09751f70e00161f1c5b.mockapi.io/postres', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload)
+      })
+        .then((res) => {
+          console.log('Postre agregado', res)
+          location.reload()
+        })
+        .catch((error) => console(error))
     }
   }
 })
